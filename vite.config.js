7549652cf948df36e86bdd9d34b8fe05dafc2417
aliatwood <aliatwood0104@gmail.com>
2026-04-19 18:02:10 +0200

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     build: {
-        outDir: 'docs'
+        outDir: 'docs',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                add: resolve(__dirname, 'add.html'),
+                about: resolve(__dirname, 'about.html')
+            }
+        }
     }
 })
